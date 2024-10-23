c1d112047dfb45a9e8d18ab3026d5e89da97d8a1
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2024-10-23 15:24:26 +0300

    add data

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Hello! It is working!");
+
+import data from './data/AllPLU.json';
+
+function convert (arr, key) {
+  arr.forEach(obj => {
+    if (obj.hasOwnProperty(key) && typeof obj[key] === 'number') {
+      obj[key] = obj[key].toString();
+    }
+  });
+
+  return arr;
+}
+
