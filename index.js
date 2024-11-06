8c0667fd701c9c81a43989756693d245dec670aa
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2024-11-06 11:25:50 +0200

    change data

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 console.log("Hello! It is working!");
 
-import data from './data/AllPLU.json';
-
 function convert (arr, key) {
   arr.forEach(obj => {
     if (obj.hasOwnProperty(key) && typeof obj[key] === 'number') {
