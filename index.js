7c395956c53cf0bfe4947d65de0deec618c4d23a
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2024-11-07 23:47:32 +0200

    add form

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 console.log("Hello! It is working!");
 
-const testData = [
-  {
-      "Article": "8000020488080",
-      "Name": " Банан mini WT",
-      "PLU": 5432
-  },
-  {
-      "Article": "8000017725840",
-      "Name": " Мідії Чорноморьскі",
-      "PLU": 682
-  },
-  {
-      "Article": "8000019620490",
-      "Name": " М'ясо Морського Гребінця AFFISH",
-      "PLU": 2990
-  }
-]
+import mongoose from 'mongoose';
 
-const listEl = document.querySelector(".search__list");
+const DB_HOST = "mongodb+srv://PLUAdmin:<db_password>@cluster0.k8ker.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect();
+
